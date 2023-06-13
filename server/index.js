1f5c5b30cbf4603f2363af7e762c456f6f3c7ea4
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-06-12 23:10:01 -0500

    [feature/documentation]: add swagger documentation

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 const app = express();
 
 // Routes
@@ -10,6 +13,8 @@ const shipmentRouter = require("./routes/ShipmentRouter");
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // LIVE
 app.get("/", (req, res) => {
--- server/routes/Common.js
+++ server/routes/Common.js
@@ -1,4 +1,5 @@
 const ganacheUrl = "http://ganache:8545";
+// const ganacheUrl = 'HTTP://127.0.0.1:7545'; // local ganache
 
 module.exports = {
   ganacheUrl
